7ea0b10f34f213b93f2cfba953ad2ba1c87d79e2
werty1st <adams@werty1st.de>
2016-04-11 18:04:26 +0200

    xml loaded

---
+++ server/XmlNewsReader.js
@@ -0,0 +1,88 @@
+"use strict";
+
+var xpathStream = require('xpath-stream');
+var http  = require("http");
+
+var url = 'http://cm2-prod-program01.dbc.zdf.de:8036/Newsflash/service/news/Nachrichten/';
+
+
+class XmlNewsReader {
+    
+    constructor (db){
+        this.db = db;
+        this.load();
+    }
+    
+    
+    /**
+     * add XML News Item to DB
+     */
+    addItem(item){
+        
+    }
+
+
+    //xml stream parser
+    /**
+     * @param {stream} stream from http get.
+     * passes sendungen to addSendetermin
+     */
+    parseXmlStream(stream){
+
+        // get Sendungen
+        stream
+            .pipe(xpathStream("/newscenter/newsflash",{
+                externalId: "id/text()",
+                subType: "subType/text()",
+                title: "title/text()",
+                text: "text/text()",
+                dateTime: "dateTime/text()"
+            }))
+            .on('data',(result)=>{
+
+                log.debug("xmlReader:", result.length,"items");
+                for(let item in result){
+                    let newsitem = result[item];
+                    this.addItem(newsitem);               
+                }           
+            });
+    }
+    
+    /**
+     * Load Data from XML Endpoint
+     */
+    load(){
+        log.info("Download:",url);
+
+        var get_options = require('url').parse(url);
+        get_options.headers = {
+                'User-Agent': process.env.npm_package_config_useragent,
+                'Cache-Control': 'no-cache'
+            };
+
+        http.get(get_options, (responeStream) => {
+
+            if (responeStream.statusCode != 200){
+                log.error(`getXmlStream: Got invalid statusCode`);
+                return;
+            } else {
+                
+                if (responeStream.headers['content-length'] == 0){
+                    log.error(`getXmlStream: Got emtpy response`);
+                    return;
+                }
+                //send to xml stream reader                   
+                this.parseXmlStream(responeStream);
+            }
+        }).on('error', (e) => {
+            log.error(`Error in response: from ${url}`);
+        });        
+    }
+    
+}
+
+
+
+
+
+module.exports = XmlNewsReader;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,48 @@
+var XmlNewsReader = require('./XmlNewsReader');
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var PouchDB = require('pouchdb');
+var winston = require('winston');
 
+global.log = new (winston.Logger)({
+    exitOnError: false,
+    transports: [
+      new (require('winston-daily-rotate-file'))({ name:"errorlog", level: "error", dirname: "logs", filename: "error.log"  }),
+      new (require('winston-daily-rotate-file'))({ name:"infolog",  level: "info", dirname: "logs", filename: "info.log"  }),
+      new (winston.transports.Console)({colorize: true, level: process.env.npm_package_config_logLevel })
+    ]
+  });
+
+log.info("Start");
+
+const db = new PouchDB(process.env.npm_package_config_database);
 const PORT = process.env.PORTS;
 
 app.use(express.static(__dirname + '/html'));
 
 io.on('connection', function(socket){
-  console.log('Picker Host is talking to me');
+  log.info('client is talking to me');
 });
 
 http.listen(PORT, function(){
-  console.log(`server is listening on *:${PORT}`);
+  log.debug(`server is listening on *:${PORT}`);
 });
+
+
+db.info().then(function (info) {
+  log.debug("db:", info);
+});
+
+// syncCouchdb
+//todo
+
+// getXMLData
+const xmlReader = new XmlNewsReader(db);
+log.debug("xmlReader loaded");
+
+
+
+
