86cc206b285a3e5d6395fa141c194d526161fbc7
werty1st <adams@werty1st.de>
2016-04-26 14:22:31 +0200

    completeFeed

---
+++ sofaapp/lib/shows/item.js
@@ -0,0 +1,24 @@
+module.exports = function(doc, req) {
+  
+    var hostname = req.headers.Host;
+
+    doc.profile = "http://zdf.de/rels/newsflash";
+    doc.self = "newsflash/" + doc._id;
+    
+    delete doc._id;
+    delete doc._rev;
+    delete doc._revisions;
+    delete doc.version;
+
+
+    provides('json', function(){
+        return toJSON(doc);
+    });    
+
+    provides('html', function(){
+        return '<pre>' + toJSON(doc) + '</pre>';
+    });
+       
+};
+
+
