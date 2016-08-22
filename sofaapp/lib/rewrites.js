25d39eacd99ca7e15de7e4ec595d26cdd188159e
werty1st <adams@werty1st.de>
2016-08-22 16:01:42 +0200

    cleanup rewrites.js

--- sofaapp/lib/rewrites.js
+++ sofaapp/lib/rewrites.js
@@ -27,33 +27,11 @@ module.exports = [
     {from:"/feed/filter/:cat/:topic",  to:'_list/feed/viewByDateCatTopic',  query: { 'listname':'Feed filtered by', 'startkey': [":cat", ":topic" ], 'endkey': [ ":cat", ":topic", {} ] }},
     
     
+
+
     /**outdated  
     {from:"/feed/outdated", to:'_list/feed/viewByDate', query: { "descending": "true", "skip": "150" }},
-    */
-    
-    
-    
-    
-    
-    
-    {from:"/today", to:'_list/list_getToday/view_getAllByDate'},
-
-    {from:"/now/:station", to:'_list/list_getNow/by_SubType',     query: { "station": ':station', "days": "0" }},
-    //{from:"/today/:station", to:'_list/list_getToday/view_getAllByDate', query: { "station": ':station', "days": "0" }},
-    {from:"/today/:station", to:'_list/list_getToday/view_getAllByStation', query: { startkey: ":station", endkey: ":station", "station": ':station', "days": "0" }},
-    
-    
-    {from:"/today/add/:days/",         to:'_list/list_getToday/view_getAllByDate', query: { station: 'all',      days: ':days' }},    
-    {from:"/today/add/:days/:station", to:'_list/list_getToday/view_getAllByDate', query: { station: ':station', days: ':days' }},    
-    
-    /*sportart
-    {from:"/today/add/:days/:sportart", to:'_list/list_getToday/view_getAllByDate', query: { station: ':station', days: ':days' }},
-    {from:"/now/:sportart", to:'_list/list_getNow/view_getAllByDate',     query: { "station": ':station', "days": "0" }},
-    {from:"/today/:sportart", to:'_list/list_getToday/view_getAllByStation', query: { startkey: ":station", endkey: ":station", "station": ':station', "days": "0" }},    
-    */
-    
-    {from:"/now/:station/:vcmsid", to:'_show/show_getByID/:vcmsid' },
-    {from:"/today/:station/:vcmsid", to:'_show/show_getByID/:vcmsid' },
-    {from:"/byId/:vcmsid", to:'_show/show_getByID/:vcmsid' },
     {from:"/*", to:'*'}
+    */
+    
 ];
